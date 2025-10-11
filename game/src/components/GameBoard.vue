8c29ece5c702af48a52f259faa19658fcd0a0fa3
Caião <caiocesarc61@gmail.com>
2025-10-10 21:43:05 -0300

    feat: Reconhecimento de clique e marcação na célula.

--- game/src/components/GameBoard.vue
+++ game/src/components/GameBoard.vue
@@ -1,10 +1,34 @@
 <template>
-    <h2>Que vença o melhor!</h2>
-    <div class="board">   
-      <div class="cell" v-for="n in 9" :key="n">{{ n }}</div>
+  <h2>Que vença o melhor!</h2>
+  <div class="board">
+    <div class="cell" v-for="(cell, index) in cells" :key="index" @click="cellClick(index)">
+      <img v-if="cell === 'O'" src="../assets/O.png" alt="Imagem O" />
+      <img v-else-if="cell === 'X'" src="../assets/X.png" alt="Imagem X" />
     </div>
+  </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      cells: Array(9).fill(""),
+      currentPlayer: 'X'
+    };
+  },
+  methods: {
+    cellClick(index) {
+      if (this.cells[index] !== "") return;
+
+      this.cells[index] = this.currentPlayer;
+
+      this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+
+
+    }
+  }
+}
+</script>
 
 <style scoped>
 h2 {
@@ -18,13 +42,13 @@ h2 {
 .board {
   display: grid;
   grid-template-columns: repeat(3, 7.5em);
-  grid-template-rows: repeat(3, 7.5em); 
+  grid-template-rows: repeat(3, 7.5em);
   grid-gap: 0px;
   margin: 2rem auto;
   width: fit-content;
 }
 
-.cell{
+.cell {
   border: 3px solid #42b983;
   cursor: pointer;
   font-size: 2em;
@@ -33,9 +57,15 @@ h2 {
   align-items: center;
   height: 100%;
 }
-.cell:hover{
+
+.cell:hover {
   background-color: #75ad94c8;
   transition: 0.5s;
   Transform: scale(1.025);
+}
+
+.cell img {
+  width: 70%;
+  height: auto;
 }
 </style>
